ac50350d6f5ac2d39c30d80204db3626a8e61acd
jojobear88 <buiquangduy@ymail.com>
2023-09-13 17:11:43 +0700

    remove unused import

--- src/components/RepoList.tsx
+++ src/components/RepoList.tsx
@@ -1,4 +1,4 @@
-import { BaseSyntheticEvent, useEffect, useMemo, useState } from "react";
+import { BaseSyntheticEvent, useMemo, useState } from "react";
 import { Input, Table } from "antd";
 import type { ColumnsType } from "antd/es/table";
 import { PiForkKnifeFill, PiStarDuotone } from "react-icons/pi";
